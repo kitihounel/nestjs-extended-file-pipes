7087f98deda11e2d2c8ccb573400363e73160bfc
kitihounel <kitihounel@gmail.com>
2025-05-03 22:36:11 +0100

    chore: linting

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,7 +1,7 @@
 import { join } from 'path';
 
-import { Test, type TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
+import { Test, type TestingModule } from '@nestjs/testing';
 
 import request from 'supertest';
 import { App } from 'supertest/types';
